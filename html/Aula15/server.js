d5dd3f0b064a8eb512197ca7f2f8f57605a8f3ad
Robson Araujo <roobson.araujo02@gmail.com>
2022-10-27 22:07:19 -0300

    config server js

--- html/Aula15/server.js
+++ html/Aula15/server.js
@@ -1,1 +1,43 @@
-console.log("Olá mundo!!")
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const porta = 8080;
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+let valor1, valor2, operacao;
+
+app.get('/processa-calc', function(req, res) {
+  valor1 = parseInt(req.query['fValor1']);
+  valor2 = parseInt(req.query['fValor2']);
+  operacao = parseInt(req.query['fOperacao']);
+
+  console.log(valor1, valor2, operacao);
+
+  processaRequisicao(res);
+});
+
+app.listen(porta,
+  () => console.log(`Servidor iniciado: ${porta}`)
+  )
+
+function processaRequisicao(res){
+  let resultado = 0;
+  switch(operacao){
+    case 1:
+      resultado = valor1 + valor2;
+      break;
+    case 2:
+      resultado = valor1 - valor2;
+      break;
+    case 3:
+      resultado = valor1 * valor2;
+      break;
+    case 4:
+      resultado = valor1 / valor2;
+      break;
+  }
+  res.send(''+ resultado);
+}
