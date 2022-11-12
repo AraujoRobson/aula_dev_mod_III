bccd9b873d7af8aa94dfa48922026fb733a66eaa
Robson Araujo <roobson.araujo02@gmail.com>
2022-11-12 14:03:01 -0300

    validate password

--- html/aula23_lista09/assets/JS/form.js
+++ html/aula23_lista09/assets/JS/form.js
@@ -7,10 +7,14 @@ form.addEventListener('submit', function(e){
   const inputEmail = e.target.querySelector('#iEmail')
   const inputBirth = e.target.querySelector('#iBirth')
   const inputSalary = e.target.querySelector('#iSalary')
+  const inputPassword = e.target.querySelector('#iPassword')
+  const inputConfirmedPassword = e.target.querySelector('#iConfirmedPassword')
 
   const name = inputName.value
   const address = inputAddress.value
   const email = inputEmail.value
+  const password = inputPassword.value
+  const confirmedPassword = inputConfirmedPassword.value
 
   let birth = new Date(inputBirth.value)
   const day = padTo2Digits(birth.getDay())
@@ -18,11 +22,22 @@ form.addEventListener('submit', function(e){
   const year = padTo2Digits(birth.getFullYear())
   birth = `${day}/${month}/${year}`
 
-  const salary = Number(inputSalary.value)
+  const salary = `R$ ${Number(inputSalary.value)}`
 
-  console.log(`${name}, ${address}, ${email}, ${birth}, ${salary} `)
+  const person = {
+    name, address, email, birth, salary
+  }
+
+  if(!validate_password(password, confirmedPassword)){
+    alert('As senhas precisam ser iguais!!')
+  } else {
+    alert('Dados Salvos!')
+    console.table(person)
+  }
+
 })
 
 // funcao para colocar 0 nas datas para ficar 08/09/2022
 let padTo2Digits = number => number.toString().padStart(2, '0')
 
+let validate_password = (password, confirmedPassword) => (password == confirmedPassword) ? true : false
