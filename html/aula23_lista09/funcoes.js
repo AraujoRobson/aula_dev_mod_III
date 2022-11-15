40dde38967a6e9a3d6864ea30f56e93b09072f9d
Robson Araujo <roobson.araujo02@gmail.com>
2022-11-15 08:44:33 -0300

    function value default

--- html/aula23_lista09/funcoes.js
+++ html/aula23_lista09/funcoes.js
@@ -1,3 +1,5 @@
+// QUESTÃO 1
+
 function par_ou_impar1(numero){
   if(numero % 2 == 0){
     return 'par'
@@ -16,3 +18,17 @@ console.log('Par ou Impar 2: ', par_ou_impar2(4));
 
 const par_ou_impar3 = numero => (numero % 2 == 0) ? 'par': 'impar'
 console.log('Par ou Impar 3: ',par_ou_impar3(43));
+
+
+
+
+
+// QUESTÃO 4
+
+function multiplicar(x = 0, y = 0){
+  return x * y
+}
+console.log(`Passando os 2 parametros: ${multiplicar(10, 5)}`);
+console.log(`Não passando nenhum parametro: ${multiplicar()}`);
+console.log(`Passando somente o valor de x: ${multiplicar(10)}`);
+console.log(`Passando somente o valor de y: ${multiplicar(undefined, 5)}`);
