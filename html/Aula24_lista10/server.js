443680ad3345bddc5e8cb1ea3be25d41c7928bcb
Robson Araujo <roobson.araujo02@gmail.com>
2022-11-16 19:58:43 -0300

    create server

---
+++ html/Aula24_lista10/server.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express();
+const port = 8080;
+
+// CONFIG CORS
+app.use(cors({origin: '*'}));
+
+app.listen(port,
+  () => console.log(`Servidor iniciado na porta: ${port}`)
+);
+
+app.get('/return-json', function(req, res){
+  const name = req.query['name']
+  const address = req.query['address']
+  const us = req.query['us']
+  const city = req.query['city']
+  const person = {
+    Nome: name,
+    Endereço: address,
+    US: us,
+    Cidade: city
+  }
+  res.status(201).json(person)
+})
