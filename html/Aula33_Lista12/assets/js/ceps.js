6187adf0ddee8d3196f127264ea8d89b138b153d
Robson Araujo <roobson.araujo02@gmail.com>
2022-12-03 08:53:00 -0300

    zip_code

---
+++ html/Aula33_Lista12/assets/js/ceps.js
@@ -0,0 +1,36 @@
+function consult(){
+  const ZIP_CODE = document.querySelector('#cep').value //'20973012'
+  const URL = `https://viacep.com.br/ws/${ZIP_CODE}/json/`
+
+  const OPTIONS = {
+    method: 'GET',
+    mode: 'cors',
+    headers: {
+      'content-type': 'application/json;charset=utf-8'
+    }
+  }
+
+
+  fetch(URL, OPTIONS)
+    .then(response => response.json())
+    .then(data => {
+      show_result(true, data);
+    })
+    .catch(err => show_result(false, err))
+}
+
+function show_result(status, data){
+  let result = document.querySelector('#result')
+  if (status){
+    if (data.err){
+      result.innerHTML = 'Não foi possivel localizar endereço!'
+    } else {
+      result.innerHTML = `<p>Endereço: ${data.logradouro}</p>
+                          <p>Complemento: ${data.complemento}</p>
+                          <p>Bairro: ${data.bairro}</p>
+                          <p>Cidade: ${data.localidade}</p>`
+    }
+  } else {
+    result.innerHTML = `Erro: ${data}`
+  }
+}
