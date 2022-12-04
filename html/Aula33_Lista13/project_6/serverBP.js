f9a18fd2d31133e1c219429cd150d50eaac42fc7
Robson Araujo <roobson.araujo02@gmail.com>
2022-12-04 09:22:53 -0300

    serverBP

---
+++ html/Aula33_Lista13/project_6/serverBP.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const port = 8080
+
+// Configure Cors
+app.use(cors({origin: '*'}))
+
+// Configures the handling of POST requests
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+// Instantiate the server
+app.listen(port, () => console.log(`Start server. port: ${port}`))
+
+// POST request handling
+app.post('/postBP', function(req, res){
+  console.log('Server: ',req.body)
+  console.log('Server: ', req.body.user)
+
+  req.body.user = req.body.user.toUpperCase()
+
+  if(req.body.user === ''){
+    res.status(404).send({})
+  } else {
+    res.status(200).send(req.body)
+  }
+})
