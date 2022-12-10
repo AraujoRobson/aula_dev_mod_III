8a66a548857c7eeaf7006584b27fd63bed69250c
Robson Araujo <roobson.araujo02@gmail.com>
2022-12-10 15:32:41 -0300

    exercise three

---
+++ html/Aula38_lista16/Exercise1/three.js
@@ -0,0 +1,6 @@
+const students = [
+  {name: 'Fulana',   grade: 7.3, scholar: false},
+  {name: 'Beltrana', grade: 9.2, scholar: true},
+  {name: 'Sicrana',  grade: 9.8, scholar: false},
+  {name: 'Maria',    grade: 8.7, scholar: true},
+]
