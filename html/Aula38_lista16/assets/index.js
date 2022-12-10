532cb6abb11dbd91fc6fef44f9f371a548807da7
Robson Araujo <roobson.araujo02@gmail.com>
2022-12-10 13:51:24 -0300

    products

---
+++ html/Aula38_lista16/assets/index.js
@@ -0,0 +1,10 @@
+const products = [
+  {name: 'Notebook Samsumg', price: 4999.99, sold: true,  type: 'notebook'},
+  {name: 'Notebook Sony',    price: 5999.99, sold: false, type: 'notebook'},
+  {name: 'iPad',             price: 6999.99, sold: false, type: 'tablet'},
+  {name: 'iPhone',           price: 7999.99, sold: true,  type: 'smartphone'},
+  {name: 'Copo de plástico', price: 1.99,    sold: false, type: 'copo'},
+  {name: 'Copo de vidro',    price: 10.99,   sold: true,  type: 'copo'},
+]
+console.table(products)
+
