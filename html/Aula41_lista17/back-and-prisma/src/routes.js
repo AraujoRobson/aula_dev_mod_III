26c8899a9f3c0a4a438a6c9fa8889151c27bb1e9
Robson Araujo <roobson.araujo02@gmail.com>
2022-12-26 15:28:17 -0300

    router post

--- html/Aula41_lista17/back-and-prisma/src/routes.js
+++ html/Aula41_lista17/back-and-prisma/src/routes.js
@@ -22,4 +22,26 @@ router.get('/', async (req, res) => {
   }
 })
 
+router.post('/', async (req, res) => {
+  const { name, email } = req.body
+
+  try{
+    let user = await User.findUnique({ where: { email }})
+    if(user){
+      return res.status(401).json({ error: 'E-mail already registered!'})
+    }
+
+    user = await User.create({
+      data: {
+        name,
+        email
+      }
+    })
+
+    res.status(200).json({ msg: 'User added successfully!'})
+  } catch (error){
+    res.json(error)
+  }
+})
+
 module.exports = router;
