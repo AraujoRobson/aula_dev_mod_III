ce9cec8e3dfe71788fbbcb1da8f8bd25cd1a6be9
Robson Araujo <roobson.araujo02@gmail.com>
2022-12-26 16:03:39 -0300

    refactored

---
+++ html/Aula41_lista17/back-and-prisma/src/controllers/userController.js
@@ -0,0 +1,39 @@
+const { PrismaClient } = require("@prisma/client")
+const { User } = new PrismaClient()
+
+module.exports = {
+  async listUsers(req, res){
+    try {
+      const users = await User.findMany({
+        select: {
+          id: true,
+          name: true,
+          email: true
+        }
+      })
+      if(users.length === 0){
+        return res.status(200).json({ mensage: "There are no registered users" })
+      }
+
+      res.status(200).json({users})
+    } catch(error){
+      res.json(error)
+    }
+  },
+
+  async createUser(req, res){
+    const { name, email } = req.body
+
+    try {
+      let user = await User.create({
+        data: {
+          name,
+          email
+        }
+      })
+      res.status(200).json({ mensage: "User successfully added", user })
+    } catch (error) {
+      res.json(error)
+    }
+  }
+}
--- html/Aula41_lista17/back-and-prisma/src/routes.js
+++ html/Aula41_lista17/back-and-prisma/src/routes.js
@@ -1,47 +1,7 @@
+const controller = require('./controllers/userController')
 const router = require('express').Router()
-const { PrismaClient } = require("@prisma/client")
 
-const { User } = new PrismaClient()
+router.get('/', controller.listUsers)
+router.post('/', controller.createUser)
 
-router.get('/', async (req, res) => {
-  try {
-    const users = await User.findMany({
-      select: {
-        id: true,
-        name: true,
-        email: true
-      }
-  })
-
-  if(users.length === 0){
-    return res.status(200).json({ msg: 'There are no registered users!' })
-  }
-  res.status(200).json({ users })
-  } catch (error){
-    res.json(error)
-  }
-})
-
-router.post('/', async (req, res) => {
-  const { name, email } = req.body
-
-  try{
-    let user = await User.findUnique({ where: { email }})
-    if(user){
-      return res.status(401).json({ error: 'E-mail already registered!'})
-    }
-
-    user = await User.create({
-      data: {
-        name,
-        email
-      }
-    })
-
-    res.status(200).json({ msg: 'User added successfully!'})
-  } catch (error){
-    res.json(error)
-  }
-})
-
-module.exports = router;
+module.exports = router
