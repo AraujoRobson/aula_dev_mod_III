79aa2424d90191acb07ad110b469a539f7479339
Robson Araujo <roobson.araujo02@gmail.com>
2022-12-26 15:27:42 -0300

    server

---
+++ html/Aula41_lista17/back-and-prisma/src/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors = require('cors')
+const formData = require('express-form-data')
+
+const app = express()
+const port = 8081
+
+app.use(cors({origin: '*'}))
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.use(formData.parse())
+
+app.use('/api/users', require('./routes'))
+
+app.get('/', (request, response) => {
+  response.status(200).send('Home Page')
+})
+
+app.listen(port, () => console.log(`Server started on port: ${port}`))
