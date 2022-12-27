b87344ed6f54b5e6f48a6b0607837db9b036318b
Robson Araujo <roobson.araujo02@gmail.com>
2022-12-27 17:49:48 -0300

    add user

---
+++ html/Aula41_lista17/front-end/assets/js/add_user.js
@@ -0,0 +1,20 @@
+let formName, formEmail
+const URL = 'http://localhost:8081/api/users'
+
+window.onload = function(e){
+  formName = document.querySelector('#iName')
+  formEmail = document.querySelector('#iEmail')
+}
+
+async function addUser(){
+  const name = formName.value
+  const email = formEmail.value
+
+  axios.post(URL, { name, email })
+    .then(res => {
+      alert(res.data.mensage)
+      console.log(res.data.user);
+      setTimeout(() => window.location.href = '/front-end', 100);
+    })
+    .catch(res => console.log(res,response.data))
+}
