ce12e79b78f8b5c61cad60230cf7b46bcf720974
Robson Araujo <roobson.araujo02@gmail.com>
2023-02-03 22:19:00 -0300

    changed to product

--- html/Aula47/front-end/assets/js/add_Product.js
+++ html/Aula47/front-end/assets/js/add_Product.js
@@ -1,19 +1,23 @@
-let formName, formEmail
-const URL = 'http://localhost:8081/api/users/'
+let formDescription, formQuantity, formId, formPrice
+const URL = 'http://localhost:8081/api/products/'
 
 window.onload = function(e){
-  formName = document.querySelector('#iName')
-  formEmail = document.querySelector('#iEmail')
+  formId = document.querySelector('#iId')
+  formDescription = document.querySelector('#iDescription')
+  formQuantity = document.querySelector('#iQuantity')
+  formPrice = document.querySelector('#iPrice')
 }
 
-async function addUser(){
-  const name = formName.value
-  const email = formEmail.value
+async function addProduct(){
+  const id = formId.value
+  const description = formDescription.value
+  const quantity = formQuantity.value
+  const price = formPrice.value
 
-  axios.post(URL, { name, email })
+  axios.post(URL, {id, description, quantity, price })
     .then(res => {
+      res.data.toString()
       alert(res.data.message)
-      console.log(res.data.user);
       setTimeout(() => window.location.href = '/front-end', 100);
     })
     .catch(res => console.log(res,response.data))
